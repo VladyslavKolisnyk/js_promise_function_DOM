08bd0f08c9c7a0abb8b17b4cab5cfef573d0ae9a
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-12-01 18:32:16 +0200

    make build

---
+++ index.1c2636ba.js
@@ -0,0 +1,3 @@
+function e(e,n){return new Promise(function(t){return e.addEventListener(n,function(){return t("It was ".concat(n," on the element: ")+"".concat(e.nodeName,", id: ").concat(e.id,"."))},{once:!1})})}var n=function(e){var n=document.createElement("div");n.textContent=e,n.classList="message",document.body.append(n)},t=document.getElementById("login"),c=document.getElementById("password"),o=document.getElementById("submit");e(t,"click").then(n),e(c,"click").then(n),e(o,"click").then(n),e(t,"input").then(n),e(c,"input").then(n),e(t,"blur").then(n),e(c,"blur").then(n),e(o,"blur").then(n);//# sourceMappingURL=index.1c2636ba.js.map
+
+//# sourceMappingURL=index.1c2636ba.js.map
